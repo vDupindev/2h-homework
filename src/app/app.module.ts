350dd234689808740d6b47153b3ed94d9552d399
vdup <victor.dupin.pro@gmail.com>
2021-05-06 10:47:03 +0200

    add sharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BackendService } from './backend.service';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, SharedModule],
     providers: [BackendService],
     bootstrap: [AppComponent]
 })
